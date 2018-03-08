1a82fdb2b66a2c5442823e588d72555971111162
Melissa Sheehan <mxsheehan7@gmail.com>
2018-03-08 13:44:48 -0800

    rolledback to dario's commit

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,28 +1,7 @@
-//Calculate the top 10 tweeters (by volume of tweets) in a given CSV file of tweets
- 
-// takes one command line argument, the path of the csv file.
-// It should gracefully handle both valid and invalid inputs. For valid inputs
-// it should produce 10 lines of output (most frequent to least frequent): <tweeter>: <count of tweets> 
-// A valid csv file will have a header, and the tweeter column will be called “name”.
-
-//Allowed assumptions:
-// max valid line length = no longer than the longest line in the csv file provided for Homework 3
-// max length of the file = 20,000 lines
-// max number of tweeters = no larger than the set of tweeters in the cl­tweets­short.csv file
-// a valid file will not have additional commas inside the tweet
-
-// Program should not crash on ANY input, even invalid ones.
-// Program should work with the provided Ubuntu afl docker, which is where it will be fuzz tested by your assigned testing team.
- 
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-bool is_valid(char *input) {
-  if(!input) {
-    return false;
-  }
 
 typedef struct {
 	char *name;
